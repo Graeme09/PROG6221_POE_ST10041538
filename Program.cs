70e0436def7c2639da74ebb24e36172268c536a3
Graeme09 <graham.poortier@gmail.com>
2023-04-11 10:46:56 +0200

    Adding Intro questions Recipe name Steps Ingredients

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,22 @@ namespace POE
     {
         static void Main(string[] args)
         {
+            methods useobj = new methods();
+            String recipeName;
+            int numOfSteps;
+            int numOfIngredients;
+
+            Console.WriteLine("Enter the name of the recipe");
+            recipeName = Console.ReadLine();
+            Console.WriteLine("Enter the amount of ingredients");
+            numOfSteps = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the amount of Steps");
+            numOfIngredients = Convert.ToInt32(Console.ReadLine());
 
 
+
+            useobj.getSteps(4);
+            useobj.getIngredients(4);
 
         }
     }
@@ -20,9 +35,36 @@ namespace POE
 
 
     class methods {
-       public int numOfIngredients { get; set; }
+        public void getSteps(int numOfSteps)
+        {
+            String stepsDesp;
+
+            for (int i = 0 + 1; i < numOfSteps + 1; i++)
+            {
+
+                Console.WriteLine("Enter the description of step " + i);
+                stepsDesp = Console.ReadLine();
+ 
+
+            }
+        }
+
+        int ingrediant;
+        public void getIngredients(int numOfIngrediants) 
+        {
 
 
+            for (int i = 1;i <numOfIngrediants +1;i++) {
+                Console.WriteLine("Enter the type of ingrediant" + i);
+                ingrediant = Convert.ToInt32(Console.ReadLine());
 
-    }
-}
+
+            }
+        
+        }
+
+
+        
+        }
+ }
+
